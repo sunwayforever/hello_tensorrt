49a6887cdf949e87f47a5704ca0796860229738b
sunway <wei.sun@hexintek.com>
2022-06-29 18:40:16 +0800

    normalize

--- ssd.cpp
+++ ssd.cpp
@@ -1,9 +1,12 @@
 #include "detnet.h"
 #include "plugin.h"
 
+#include "kernel/normalize_plugin.h"
+
 int main(int argc, char** argv) {
     // REGISTER_ALL_PLUGINS;
-    DetNet net("model/ssd.prototxt", "model/ssd.caffemodel", "detection_out");
+    REGISTER_TENSORRT_PLUGIN(NormalizePluginCreator);
+    DetNet net("model/ssd.prototxt", "model/ssd.caffemodel", "conv4_3_norm");
     net.build();
     net.infer();
     net.teardown();
