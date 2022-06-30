2f866e176e660e2a55c36f5c5778dbab752079b5
sunway <wei.sun@hexintek.com>
2022-06-30 19:00:18 +0800

    prior_box

--- ssd.cpp
+++ ssd.cpp
@@ -2,10 +2,21 @@
 #include "plugin.h"
 
 int main(int argc, char** argv) {
-    REGISTER_ALL_PLUGINS;
-    DetNet net(
-        "model/ssd.prototxt", "model/ssd.caffemodel",
-        "conv4_3_norm_mbox_priorbox");
+    // REGISTER_ALL_PLUGINS;
+    REGISTER_TENSORRT_PLUGIN(EltwisePluginCreator);
+    REGISTER_TENSORRT_PLUGIN(BatchNormPluginCreator);
+    REGISTER_TENSORRT_PLUGIN(ConvolutionPluginCreator);
+    REGISTER_TENSORRT_PLUGIN(InnerProductPluginCreator);
+    REGISTER_TENSORRT_PLUGIN(LRNPluginCreator);
+    REGISTER_TENSORRT_PLUGIN(PoolingPluginCreator);
+    REGISTER_TENSORRT_PLUGIN(PowerPluginCreator);
+    REGISTER_TENSORRT_PLUGIN(ReluPluginCreator);
+    REGISTER_TENSORRT_PLUGIN(ScalePluginCreator);
+    // REGISTER_TENSORRT_PLUGIN(SoftmaxPluginCreator);
+    REGISTER_TENSORRT_PLUGIN(Normalize2PluginCreator);
+    REGISTER_TENSORRT_PLUGIN(PriorBox2PluginCreator);
+
+    DetNet net("model/ssd.prototxt", "model/ssd.caffemodel", "detection_out");
 
     net.build();
     net.infer();
